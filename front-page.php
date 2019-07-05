3396e022c0291040509e7d8d3fefd26efca5143c
Nick Cartwright <dominic.cartwright07@gmail.com>
2019-07-04 23:09:58 -0400

    Update text and spacing

--- front-page.php
+++ front-page.php
@@ -4,10 +4,10 @@
 
 	<div class="container container-lg">
 		<div class="row">
-			<div class="col-lg-6 pt-5">
-				<h2 class="font-weight-bold text-10 line-height-2 appear-animation" data-appear-animation="fadeInRightShorter" data-appear-animation-delay="500"><span class="text-5">Hello, I’m John Doe. I’m a Digital Designer From New York City.</span></h2>
-				<h4 class="text-6 line-height-6 font-weight-normal appear-animation" data-appear-animation="fadeInRightShorter" data-appear-animation-delay="800"><span class="opacity-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit phasellus blandit massa enim adipiscing elit phasellus.</span></h4>
-				<a class="btn btn-primary btn-with-arrow mb-2 ml-0 pl-0 appear-animation" data-appear-animation="fadeInRightShorter" data-appear-animation-delay="1100" href="#">Contact Us <span><i class="fas fa-chevron-right"></i></span></a>
+			<div class="col-lg-7 pt-5">
+				<h2 class="font-weight-bold text-10 line-height-2 appear-animation" data-appear-animation="fadeInRightShorter" data-appear-animation-delay="500"><span class="text-5">Hello, I’m Dominic Cartwright. I’m a Web Developer From The Bahamas.</span></h2>
+				<h4 class="text-6 line-height-6 font-weight-normal appear-animation" data-appear-animation="fadeInRightShorter" data-appear-animation-delay="800"><span class="opacity-6">If you are looking some help with your web presence, I can help.</span></h4>
+				<a class="btn btn-primary btn-with-arrow mb-2 ml-0 pl-0 appear-animation" data-appear-animation="fadeInRightShorter" data-appear-animation-delay="1100" href="/contact">Contact Me <span><i class="fas fa-chevron-right"></i></span></a>
 			</div>
 		</div>
 	</div>
