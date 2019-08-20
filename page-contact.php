3432033036eb4d352831e06bf9a55151d5199d54
Nick Cartwright <dominic.cartwright07@gmail.com>
2019-08-19 23:39:10 -0400

    Change us to me

--- page-contact.php
+++ page-contact.php
@@ -7,13 +7,13 @@
 			<div class="row">
 
 				<div class="col-md-8 order-2 order-md-1 align-self-center p-static">
-					<h1 class="text-dark">Contact Us</h1>
+					<h1 class="text-dark">Contact Me</h1>
 				</div>
 
 				<div class="col-md-4 order-1 order-md-2 align-self-center">
 					<ul class="breadcrumb d-block text-md-right text-dark">
 						<li><a href="/">Home</a></li>
-						<li class="active">Contact Us</li>
+						<li class="active">Contact Me</li>
 					</ul>
 				</div>
 
