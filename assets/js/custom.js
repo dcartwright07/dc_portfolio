addca6eff3226012cb601fe124ca167dab3415e7
Nick Cartwright <dominic.cartwright07@gmail.com>
2019-08-20 20:56:07 -0400

    Cleaned custom.js

--- assets/js/custom.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-jQuery(function () {
-  'use strict';
-
-  jQuery('[data-toggle="offcanvas"]').on('click', function () {
-    jQuery('.offcanvas-collapse').toggleClass('open');
-  });
-});
