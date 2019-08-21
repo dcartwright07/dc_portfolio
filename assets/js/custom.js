f1e7384f308066b971d18bdbf0a095148863e29d
Nick Cartwright <dominic.cartwright07@gmail.com>
2019-08-20 20:52:39 -0400

    Fix positioning of mobile menu

---
+++ assets/js/custom.js
@@ -0,0 +1,9 @@
+"use strict";
+
+jQuery(function () {
+  'use strict';
+
+  jQuery('[data-toggle="offcanvas"]').on('click', function () {
+    jQuery('.offcanvas-collapse').toggleClass('open');
+  });
+});
